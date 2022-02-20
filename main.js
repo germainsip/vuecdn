56c2504b7e827a50d968ac516c154aa1b2a5388d
Germain SIPIERE <gsipiere@gmail.com>
2022-02-20 19:00:58 +0100

    fin list

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ const app = Vue.createApp({
             product: 'Chaussette',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% coton', '30% laine', '20% polyester']
+            details: ['50% coton', '30% laine', '20% polyester'],
+            variants: [
+                {id: 1234 , color: 'bleu'},
+                {id: 3245 , color: 'vert'}
+            ]
         }
     }
 })
