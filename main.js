2766a7eb626ce9360b8eb8847d4acd51dc0f559e
Germain SIPIERE <gsipiere@gmail.com>
2022-02-20 16:56:45 +0100

    debut conditions

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const app = Vue.createApp({
         return {
             product: 'Chaussettes',
             image: './assets/images/socks_blue.jpg',
-            url: 'https://www.google.com/search?q=chaussettes+images&sxsrf=APq-WBvF9v1Mt59W275dpsvSA9iu85J4Fw:1645357328623&source=lnms&tbm=isch&sa=X&ved=2ahUKEwip9ObYmY72AhWlxIUKHYVxBqkQ_AUoAnoECAEQBA&biw=1322&bih=968&dpr=2'
 
         }
     }
