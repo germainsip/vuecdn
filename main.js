2bd6efd8e23e21c8d17c7ce3f6d23215a708d5c5
Germain SIPIERE <gsipiere@gmail.com>
2022-02-21 12:05:55 +0100

    fin

--- main.js
+++ main.js
@@ -7,9 +7,23 @@ const app = Vue.createApp({
             inStock: true,
             details: ['50% coton', '30% laine', '20% polyester'],
             variants: [
-                {id: 1234 , color: 'bleu'},
-                {id: 3245 , color: 'vert'}
+                {id: 1234 , color: 'bleu', image: './assets/images/socks_blue.jpg'},
+                {id: 3245 , color: 'vert', image: './assets/images/socks_green.jpg'}
             ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        remFromCart() {
+            if(this.cart > 0) {
+                this.cart -= 1
+            }
+            
+        }, 
+        changeImg(variantImage) {
+            this.image = variantImage
         }
     }
 })
